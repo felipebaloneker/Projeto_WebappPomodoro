0f23c9d91cb07ee0233bc9da5258e9d32b17b5af
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-04 13:09:07 -0300

    Começando de novo adicionado timer string e buttao de start como alterado texto do butão ao clicar

--- script.js
+++ script.js
@@ -1,54 +1,15 @@
-// Menus do pomodoro
-var strtimer = document.getElementById('minutos');
-var interface = document.querySelector('.interface');
-var timerbt =  document.querySelectorAll('.timer-bt')
-var btpomodoro = document.getElementById('pomodoro');
-var btintervalo = document.getElementById('intervalo');
-var btintlong = document.getElementById('intlong');
-var min =25;
-// Pagina Timer
-btpomodoro.addEventListener('click', function(event){
-  interface.style.background = '#890C06';
-  var i;
-  for(i = 0; i< timerbt.length; i++){
-    timerbt[i].style.backgroundColor = '#BC0B05';
+var minute = 25;
+var sec = 00;
+var string = document.getElementById('string');
+var btnStart = document.getElementById('start');
+function Começar(){
+  /* Trocando texto do botão ao clicar */
+  if(btnStart.textContent === 'Começar'){
+    btnStart.textContent = 'Pausar';
   }
-  strtimer.innerHTML = '25:00';
-})
-
-// Pagina Pausa
-btintervalo.addEventListener('click', function(event){
-  interface.style.background = "#3089AA";
-  var i;
-  for(i = 0; i< timerbt.length; i++){
-    timerbt[i].style.backgroundColor = '#3089AA';
+  else{
+    btnStart.textContent = 'Começar';
   }
-  strtimer.innerHTML = '5:00';
-  min = 5;
-})
-// Pagina Pausa Longa
-btintlong.addEventListener('click', function(event){
-  interface.style.background = "#294D4B";
-  var i;
-  for(i = 0; i< timerbt.length; i++){
-    timerbt[i].style.background = '#294D4B';
-  }
-  strtimer.innerHTML = '15:00';
-  min = 15;
-})
-  // Temporizador
-  function Timer(){
-    var sec = 0;
-    if(min > 0){
-        setInterval(function(){
-            if(sec > 0){
-                sec-=1;
-                strtimer.innerHTML = `${min}:${sec}`;
-            }
-            else if(sec == 0){
-                sec += 60;
-                min-=1;
-            }
-        },1000)
-    }
+  /**/
 }
+btnStart.addEventListener('click',Começar);
