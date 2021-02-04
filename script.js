976a2cde256410a26c0d4b01f169e8a908788fd3
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-04 16:42:11 -0300

    timer funcionando assim como pausa

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-var minute = 25;
-var sec = 00;
+var active = false;
+var timer = {};
+var min = 24;
+var sec = 59;
+var minutes;
 var string = document.getElementById('string');
 var btnStart = document.getElementById('start');
-function Começar(){
+/* Ao clicar no botão Começar */
+function Start(){
   /* Trocando texto do botão ao clicar */
   if(btnStart.textContent === 'Começar'){
     btnStart.textContent = 'Pausar';
+    timerStart();
   }
   else{
     btnStart.textContent = 'Começar';
+    timerStop();
   }
-  /**/
 }
-btnStart.addEventListener('click',Começar);
+  /* Começando contagem*/
+  function timerStart(){
+    if(active) return;
+    active = true;
+    minutes = setInterval(function second(){
+        if(sec == 0){sec+=59;min-=1;}
+        else if(sec == 0 && min == 0){clearInterval}
+        else{sec-=1;string.textContent = `${min}:${sec}`;}
+    },1000)
+}
+  /* Pausando Contagem*/
+  function timerStop(){clearInterval(minutes);}
+btnStart.addEventListener('click',Start);
---
+++ test.js
@@ -0,0 +1,2 @@
+// apos 25 minutos uma pausa apos 4x25 pausa longa
+// Segundos
