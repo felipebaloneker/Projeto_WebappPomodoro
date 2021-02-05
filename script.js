bff903f03db60870cfbe21e5f8ed467886ddb1f3
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-04 23:55:03 -0300

    adicionado cotagem de pausas e notificação sonora

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var minut; // valor inicial do min para reset
 var second; // valor inicial do sec para reset
 var minutes;
 var cont = 0;
+var mensage;
 var string = document.getElementById('string');
 var btnStart = document.getElementById('start');
 var btnReset = document.getElementById('reset');
@@ -20,18 +21,12 @@ function Start(){
     timerStop();
   }
 }
-
 /* ---Contagem regressiva---*/
     /* Começando contagem*/
     function timerStart(){
-      minut = min; // guardando valores iniciais
-      second = sec;
       minutes = setInterval(function(){
           if(sec == 0){sec+=60;min-=1;}
-          else if(min < 0){
-            if(cont < 4){Pausa();}
-            else{PausaLonga();}
-          }
+          else if(min < 0){Notify();}
           else{sec-=1;string.innerHTML = `${min}:${sec}`;}
       },1000)
   }
@@ -55,24 +50,43 @@ var btnPausal = document.getElementById('pausal');
 function Pomodoro(){
   min = 25;
   sec = 0;
+  minut = min; // guardando valores iniciais
+  second = sec;
   string.textContent = `${min}:${sec}0`;
   pgPrinc.style.background = '#C94040';
+  mensage = 'Faça uma pausa!';
 }
 btnPomodoro.addEventListener('click',Pomodoro);
     /* Pausa */
 function Pausa(){
   min = 5;
   sec = 0;
+  minut = min; 
+  second = sec;
   string.textContent = `0${min}:${sec}0`;
   pgPrinc.style.background = '#55ADA9';
-  cont ++;
+  if(cont < 4){cont ++;Circle();}
+  mensage = 'Ao trabalho!';
 }
 btnPausa.addEventListener('click',Pausa);
   /* Pausa Longa */
 function PausaLonga(){
   min = 15;
   sec = 0;
+  minut = min; 
+  second = sec;
   string.textContent = `${min}:${sec}0`;
   pgPrinc.style.background = '#3089AA';
+  mensage = 'O tempo Acabou!';
 }
 btnPausal.addEventListener('click',PausaLonga);
+/* Notificação */
+function Notify(){
+  var audio = new Audio('notify/sound.mp3');
+  audio.play();
+}
+/* Circulo pausa long */
+var circle= document.getElementsByClassName('circle');
+function Circle(){
+    circle[cont-1].style.backgroundColor ='#2C3230';
+}
