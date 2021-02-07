a04d5c61fe639371ceb25ce48953e8e7228d0bd4
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-07 17:10:56 -0300

    iniciando função Pomodoro ao carregar a pagina

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var min = 25;
+var min = 0;
 var sec = 0;
 var minut; // valor inicial do min para reset
 var second; // valor inicial do sec para reset
@@ -9,6 +9,7 @@ var string = document.getElementById('string');
 var btnStart = document.getElementById('start');
 var btnReset = document.getElementById('reset');
 var pgPrinc = document.getElementById('pg-princ');
+document.load = Pomodoro();
 /* ---Ao clicar no botão Começar---*/
 function Start(){
   /* Trocando texto do botão ao clicar */
@@ -43,9 +44,7 @@ function Start(){
     btnReset.addEventListener('click',timerReset)
 
 /* Menus do app */
-var btnPomodoro = document.getElementById('pomodoro');
-var btnPausa = document.getElementById('pausa');
-var btnPausal = document.getElementById('pausal');
+var btnMenu = document.querySelectorAll('.btn-menu');
     /* Pomodoro */
 function Pomodoro(){
   min = 25;
@@ -56,7 +55,7 @@ function Pomodoro(){
   pgPrinc.style.background = '#C94040';
   mensage = 'Faça uma pausa!';
 }
-btnPomodoro.addEventListener('click',Pomodoro);
+btnMenu[0].addEventListener('click',Pomodoro);
     /* Pausa */
 function Pausa(){
   min = 5;
@@ -68,7 +67,7 @@ function Pausa(){
   if(cont < 4){cont ++;Circle();}
   mensage = 'Ao trabalho!';
 }
-btnPausa.addEventListener('click',Pausa);
+btnMenu[1].addEventListener('click',Pausa);
   /* Pausa Longa */
 function PausaLonga(){
   min = 15;
@@ -79,13 +78,13 @@ function PausaLonga(){
   pgPrinc.style.background = '#3089AA';
   mensage = 'O tempo Acabou!';
 }
-btnPausal.addEventListener('click',PausaLonga);
+btnMenu[2].addEventListener('click',PausaLonga);
 /* Notificação */
 function Notify(){
   var audio = new Audio('notify/sound.mp3');
   audio.play();
 }
-/* Circulo pausa long */
+/* Contagem até pausa longa */
 var circle= document.getElementsByClassName('circle');
 function Circle(){
     circle[cont-1].style.backgroundColor ='#2C3230';
